3d482693ae0f0eaf9cdda1c32825564a581d8c6b
danielkhalils <dkses.hef@uea.edu.br>
2022-12-05 20:43:31 -0400

    alterando rotas de páginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NewMomentComponent } from './components/pages/sms-sender/sms-sender.component';
 import { HomeComponent } from './components/home/home.component';
-import { MomentFormComponent } from './components/sms-sender-form/sms-sender-form.component';
+import { MomentFormComponent } from './components/pages/sms-sender-form/sms-sender-form.component';
 
 @NgModule({
   declarations: [
